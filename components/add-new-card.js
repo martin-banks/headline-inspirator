40e630e8ccc715cf50cbccf5255073b4f59f81cc
martin-banks <anotherbanksy@gmail.com>
2021-02-20 00:34:32 +1100

    styles add button

--- components/add-new-card.js
+++ components/add-new-card.js
@@ -5,6 +5,14 @@ const Wrapper = Styled.button`
   display: grid;
   justify-content: center;
   align-items: center;
+  background: none;
+  border: dashed 2px rgba(0,0,0, 0.1);
+  border-radius: 16px;
+  cursor: pointer;
+  transform: scale(0.98);
+  &:hover {
+    background: rgba(0,0,0, 0.1);
+  }
 `
 
 function AddNewCard () {
