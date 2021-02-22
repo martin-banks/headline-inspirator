6a8a862715c160dc576585105616c70b72784e70
martin-banks <anotherbanksy@gmail.com>
2021-02-22 19:50:58 +1100

    moves styles to external css file

--- pages/index.js
+++ pages/index.js
@@ -57,31 +57,6 @@ export default function Home () {
         </a>
       </Footer>
 
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-      `}</style>
     </Container>
   )
 }
