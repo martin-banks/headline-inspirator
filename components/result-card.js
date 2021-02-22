a2529a5c602b5f8c0f3b347da1807742ad9c6073
martin-banks <anotherbanksy@gmail.com>
2021-02-22 19:50:01 +1100

    adds support to change quieries for each card

--- components/result-card.js
+++ components/result-card.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import Styled from 'styled-components'
 
-import queries from '../queries'
+// import queries from '../queries'
+import queries from '../queries-new.json'
 
 
 const CardWrapper = Styled.div`
-  outline: solid 1px lightgrey; 
+  outline: solid 1px lightgrey;
   display: grid;
 `
 const Ul = Styled.ul`
@@ -33,10 +34,12 @@ const Back = Styled.div`
 
 function Card (props) {
   const {
+    index,
     type,
     title,
     handleResultClick,
     results,
+    handleQueryChange,
   } = props
 
   const [ flipped, setFlipped ] = useState(false)
@@ -49,17 +52,33 @@ function Card (props) {
   }
   const handleSelectQuery = e => {
     console.log(e.target.value)
+    let newQuery = queries.filter(q => {
+      console.log(`Looking for ${e.target.value} |`, q)
+      return q.type === e.target.value
+    })[0]
+    // for (let querySet of queries) {
+    //   for (let query of querySet.queries) {
+    //     if (query.type = e.target.value) {
+    //       newQuery = query
+    //       break
+    //     }
+    //   }
+    // }
+  
+
+    console.log({ newQuery })
+    handleQueryChange({ type, newQuery, index })
   }
 
   return (
     <CardWrapper>
       { !flipped &&
         <Front>
-          <button onClick={ showBack } >Flip</button>
-          <h3>{ title }</h3>
+          <button onClick={ showBack } >Settings</button>
+          <h3>{ results.name }</h3>
           <Ul>
-            { results[type].length
-              ? results[type].map((r, i) => <Li key={ `result-item-${type}-${i}` } onClick={ handleResultClick }>{ r.word }</Li>)
+            { results.data.length
+              ? results.data.map((r, i) => <Li key={ `result-item-${type}-${i}` } onClick={ handleResultClick }>{ r.word }</Li>)
               : <p><i>No matches found</i></p>
             }
           </Ul>
@@ -68,17 +87,35 @@ function Card (props) {
 
       { flipped &&
         <Back>
-          <button onClick={ showFront } >Flip</button>
-          <h3>Card settings</h3>
+          <button onClick={ showFront } >Settings</button>
+          <h3>Card settings for { results.name }</h3>
 
           <h4>Choose search query</h4>
           <select onChange={ handleSelectQuery }>
-            { queries
+            {/* { queries
               .map(group => group.queries
-                .map((q, i) => <option key={`option-${q.name}-${i}`} value={ i }>{ q.name }</option>)
+                .map((q, i) => <option
+                  key={`option-${q.name}-${i}`}
+                  value={ i }
+                >{ q.name }</option>)
               )
+            } */}
+            {/* {
+              Object.keys(queries).map(k => queries[k]().queries.map(query => <option
+                key={ `${option}-${query.name}` }
+                value={ query.type }
+              >{ query.name}</option>))
+            } */}
+            {
+              queries.map((q, i) => <option
+                key={ `${q.type}--${i}` }
+                value={ q.type }
+                selected={ type === q.type }
+              >{ q.name }</option>)
             }
           </select>
+
+          <p><i>{ results.description }</i></p>
 
 
         </Back>
