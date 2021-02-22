0e0d523b723ec9a98f56d9973798b2c6f0bd3921
martin-banks <anotherbanksy@gmail.com>
2021-02-22 19:50:41 +1100

    queries controlled by new external DB, allows for query changes for each card

--- components/search-input.js
+++ components/search-input.js
@@ -6,6 +6,9 @@ import Card from '../components/result-card'
 import Loading from '../components/loading'
 import AddNewCard from '../components/add-new-card'
 
+// import queries from '../queries'
+import queries from '../queries-new.json'
+
 
 const InputContainer = Styled.section`
   display: grid;
@@ -44,15 +47,20 @@ function SearchInput (props) {
   const [ results, storeResults ] = useState({})
   const [ isLoading, setIsLoading ] = useState(false)
 
+  const [ userQueries, updateQueries ] = useState([])
+
+  useEffect(() => {
+    updateQueries(prev => {
+      // const update = [ ...prev ]
+      // update[0] = (queries[0])
+      return [ queries[0] ]
+    })
+  }, [])
+
   const inputRef = useRef()
 
-  const queries = {
-    // ml: 'Similar in meaning',
-    topics: 'Related topics',
-    rel_rhy: 'Rhymes with',
-  }
 
-  const endPoint = `https://api.datamuse.com/words?`
+  // const endPoint = `https://api.datamuse.com/words?`
   const limit = '10'
 
   const handleSubmit = e => {
@@ -74,31 +82,101 @@ function SearchInput (props) {
       storeSearchWords(() => keywords)
     }
   }
+  const changecardQuery = ({ index, type, newQuery }) => {
+    updateQueries(prev => {
+      const update = [ ...prev ]
+      update[index] = newQuery
+      console.log({ update })
+      return update
+    })
+  }
 
   useEffect(async () => {
     if (searchWords) {
       setIsLoading(true)
 
-      for (let type of Object.keys(queries)) {
-        await fetch(`${endPoint}${type}=${searchWords.join('+')}&max=${limit}`)
+      // for (let type of Object.keys(queries)) {
+      //   await fetch(`${endPoint}${type}=${searchWords.join('+')}&max=${limit}`)
+      //     .then(res => res.json())
+      //     .then(data => {
+      //       storeResults(prev => ({ ...prev, [type]: data }))
+      //     })
+      //     .catch(err => {
+      //       setIsLoading(false)
+      //     })
+      //   }
+
+        // searchWords.join('+')}&max=${limit}
+
+      // for (let queryKey of Object.keys(queries)) {
+      //   try {
+      //     // Build all of the queries for this entry in the database list
+      //     console.log({queryKey}, queries[queryKey])
+      //     const querySet = queries[queryKey]({ words: searchWords.join('+'), max: limit })
+      //     // Itterate all of the queries for that database
+      //     for (let q of querySet.queries) {
+      //       const { name, description, type } = q
+      //       await fetch(q.query)
+      //         .then(res => res.json())
+      //         .then(data => {
+  
+      //           const toStore = { data, name, description }
+      //           console.log({ toStore })
+      //           storeResults(prev => ({ ...prev, [type]: toStore }))}
+      //         )
+      //         .catch(err => console.error('--- ERROR FETCHING DATA ---\n', err))
+      //     }
+      //     console.error('--- ERROR CREATING QUERY SET ---\n', err)
+      //   } catch (err) {
+      //   }
+      // }
+
+
+      // for (let querySet of queries) {
+      //   const { formula } = querySet
+
+      //   for (let q of querySet.queries) {
+      //     const { type, name, description, query  } = q
+      //     const url = formula
+      //       .replace('<query>', query)
+      //       .replace('<words>', searchWords.join('+'))
+      //       .replace('<max>', limit)
+      //     fetch(url)
+      //       .then(res => res.json())
+      //       .then(data => {
+      //         const toStore = { data, name, description }
+      //         console.log({ toStore })
+      //         storeResults(prev => ({ ...prev, [type]: toStore }))
+      //       })
+      //   }
+      // }
+
+      storeResults([])
+
+      for (let query of userQueries) {
+        console.log({ query })
+        const { formula, name, description, type } = query
+        const url = formula
+          .replace('<words>', searchWords.join('+'))
+          .replace('<max>', limit)
+
+        fetch(url)
           .then(res => res.json())
           .then(data => {
-            storeResults(prev => ({ ...prev, [type]: data }))
+            const toStore = { data, name, description, type }
+            console.log({ toStore })
+            storeResults(prev => ([ ...prev, toStore ]))
           })
-          .catch(err => {
-            setIsLoading(false)
-          })
-        }
-
-        setIsLoading(false)
+      }
+      setIsLoading(false)
     }
-  }, [ searchWords ])
+  }, [ searchWords, userQueries ])
 
   return (
     <section>
       <InputContainer>
         <h2>Enter keywords to begin</h2>
-        <p><i>No punctuation, number or special characters. Lowercase only</i></p>
+        <p><i>No punctuation, numbers or special characters</i></p>
 
         <form onSubmit={ handleSubmit }>
           <TextInput ref={ inputRef } type="text" name="keywords" id="keywords" readOnly={ isLoading }/>
@@ -106,15 +184,32 @@ function SearchInput (props) {
         </form>
       </InputContainer>
 
-      { Object.keys(results).length
+      {/* { Object.keys(results).length
         ? <CardContainer>
           {
             Object.keys(results).map(k => <Card
-              key={ `result-card-${queries[k]}` }
+              key={ `result-card-${results.type}` }
+              index={ 0 }
               handleResultClick={ handleSubmitFromResult }
-              results={ results }
-              title={ queries[k] }
+              handleQueryChange={ changecardQuery }
+              results={ results[k] }
               type={ k }
+            />)
+          }
+          <AddNewCard />
+        </CardContainer>
+        : ''
+      } */}
+      { results.length
+        ? <CardContainer>
+          {
+            results.map(result => <Card
+              key={ `result-card-${result.type}` }
+              index={ 0 }
+              handleResultClick={ handleSubmitFromResult }
+              handleQueryChange={ changecardQuery }
+              results={ result }
+              type={ result.type }
             />)
           }
           <AddNewCard />
@@ -122,7 +217,8 @@ function SearchInput (props) {
         : ''
       }
 
-      {/* { results && <Dump>{ JSON.stringify(results, null, 2) }</Dump> } */}
+      { results && <Dump>{ JSON.stringify(userQueries, null, 2) }</Dump> }
+      { results && <Dump>{ JSON.stringify(results, null, 2) }</Dump> }
 
       { isLoading && <Loading /> }
 
